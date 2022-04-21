bef02094b3d22a79f7c76c86490e7a133a32a41c
ParenteMauro <mauroparenteinformatica@gmail.com>
2022-04-21 09:18:04 -0300

    Tipos de datos

---
+++ primerPrograma/miPrimerPrograma.js
@@ -0,0 +1,19 @@
+
+var saludar = "Hola Mundo"
+
+console.log(saludar)
+
+
+saludar = "cambiado" //String
+
+console.log(saludar)
+
+var dinero = 250
+
+var casado = true
+
+alert("hola")
+
+var valorIngresado = prompt("Ingrese un valor") //camelCase
+
+
