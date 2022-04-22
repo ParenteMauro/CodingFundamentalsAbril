80cb12a7edcf0da38f1c3ec3c14befa91b5f9632
ParenteMauro <mauroparenteinformatica@gmail.com>
2022-04-22 09:39:54 -0300

    Conversión de tipos de Datos

---
+++ Conversion de datos/calcularImpuesto.js
@@ -0,0 +1,22 @@
+var dinero = prompt("Ingrese tu dinero") 
+
+var impuesto = dinero * 0.21
+
+console.log(impuesto)
+//conversión de datos implicita, es cuando JS hace esta conversión sin especificarla
+
+
+//La conversión de datos explícita es aquella que damos las instrucciones de que haga la conversión especificando con código al programa
+//Ejemplo: var dinero = parseInt(prompt("Ingrese tu dinero"))
+
+
+var saludo = "Hola mundo"
+
+var cambiar = parseInt(saludo)//Nan = Not a Number
+
+
+console.log(cambiar)
+
+
+
+
